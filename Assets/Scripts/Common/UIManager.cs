d771add7a7ccdc73a398510e7c238bb76d7f2d7d
chinatragedy <1146547962@qq.com>
2017-12-02 13:20:19 +0800

    [mod] 修改乱码

--- Assets/Scripts/Common/UIManager.cs
+++ Assets/Scripts/Common/UIManager.cs
@@ -3,34 +3,48 @@ using System.Collections.Generic;
 
 public class UIManager
 {
-    #region 单例模式
-    private UIManager() { }
-    private static UIManager _instance;
-    public static UIManager Instance
-    {
-        get { return _instance ?? (_instance = new UIManager()); }
-    }
-    #endregion
+	#region 单例模式
+	private UIManager() { }
+	private static UIManager _instance;
+	public static UIManager Instance
+	{
+		get { return _instance ?? (_instance = new UIManager()); }
+	}
+	#endregion
 
-    private Dictionary<string, UIBase> pageDic = new Dictionary<string, UIBase>();
+	private Dictionary<string, UIBase> pageDic = new Dictionary<string, UIBase>();
 
-    public UIBase ShowView(string name)
-    {
-<<<<<<< HEAD
-        UIBase page = Resources.Load(name, typeof(UIBase)) as UIBase;
+	public UIBase ShowView(string name)
+	{
+		//UIBase page = Resources.Load(name, typeof(UIBase)) as UIBase;
+		UIBase page = null;
+		if (pageDic.TryGetValue(name, out page))
+		{
+			page.gameObject.SetActive(true);
+			return page;
+		}
+		GameObject prefab = Resources.Load(name, typeof(GameObject)) as GameObject;
+		GameObject obj = Object.Instantiate(prefab);
+		obj.name = name;
+		page = obj.GetComponent<UIBase>();
+		GameObject parentObj = GameObject.Find("UI Root");
+		if (null != parentObj)
+		{
+			obj.transform.SetParent(parentObj.transform, false);
+		}
 
-=======
-        //UIBase page = Resources.Load(name, typeof(UIBase)) as UIBase;
-        GameObject prefab = Resources.Load(name, typeof(GameObject)) as GameObject;
-        GameObject obj = Object.Instantiate(prefab);
-        obj.name = name;
-        UIBase page = obj.GetComponent<UIBase>();
-        GameObject parentObj = GameObject.Find("UI Root");
-        if(null != parentObj)
-        {
-            obj.transform.SetParent(parentObj.transform, false);
-        }
->>>>>>> parent of 12681cf... Add Bomb
-        return page;
-    }
+		pageDic[name] = page;
+		return page;
+	}
+
+	public UIBase HideView(string name)
+	{
+		UIBase page = null;
+		if (pageDic.TryGetValue(name, out page))
+		{
+			page.gameObject.SetActive(false);
+		}
+
+		return page;
+	}
 }
